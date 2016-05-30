d377eb51d26ff26a8f578097cc3bb3a1d64c539a
tecnogab <tecnogab.oran@gmail.com>
2016-05-30 20:22:46 -0300

    30-05-2016 20:22:54

--- php/domicilios/domicilios.php
+++ php/domicilios/domicilios.php
@@ -24,3 +24,8 @@
 		}
 	}		
 ?>
+<?PHP
+
+SELECT t_clientes.nombre_cli, t_domicilios.barrio_dom, t_domicilios.calle_dom, AsText(t_domicilios.coordenadas_dom) 
+FROM t_domicilios, t_clientes WHERE t_domicilios.id_cli = t_clientes.id_cli AND t_domicilios.id_cli = 3
+?>
